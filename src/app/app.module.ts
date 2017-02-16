ea4210787cf4b27821583fe56edf8989386447a7
lebontonii <lebontonii@gmail.com>
2017-02-16 19:29:49 +0100

    Integrated Angular 2 with Express API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,40 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+//Component creat amb ng generate component users
+import { UsersComponent } from './users/users.component';
+//Servei per interactuar amb la API d'usuaris
+import { UsersService } from './users.service';
+
+//Definim les routes de la nostra app
+const ROUTES = [
+//Si es visita el root redireccionar a /users --CANVIAR EN UN FUTUR PER EL LOGIN BLABLABLA
+  {
+    path: '',
+    redirectTo: 'users',
+    pathMatch: 'full'
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [UsersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
