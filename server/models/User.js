16aee05e3b39b471af6da15039fc93337c8e1eca
lebontonii <lebontonii@gmail.com>
2017-03-09 17:00:33 +0100

    New User Attributes

--- server/models/User.js
+++ server/models/User.js
@@ -2,8 +2,18 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var UserSchema   = new Schema({
-    name: String,
-    categories: [{type: Schema.ObjectId, ref: "Category"}]
+   email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  categories: [{type: Schema.ObjectId, ref: "Category"}]
+  hash: String,
+  salt: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
